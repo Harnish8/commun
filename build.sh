4626f25e5acec9645d5fd12f7eca5dfb1071ee18
Harnish8 <concept.harnish@gmail.com>
2026-02-19 20:35:48 +0530

    Add build.sh script

---
+++ build.sh
@@ -0,0 +1,6 @@
+﻿#!/usr/bin/env bash
+set -e
+
+pip install emergentintegrations --extra-index-url https://d33sy5i8bnduwe.cloudfront.net/simple/ --trusted-host d33sy5i8bnduwe.cloudfront.net
+
+pip install -r backend/requirements.txt
